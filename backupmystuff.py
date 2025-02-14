a2c0b9674b02fa050af9d9fc9db5d7abe88606e6
beardedkwan <amar536@protonmail.com>
2025-02-14 13:02:44 -0500

    Added backup for PS1 variable and MangoHud config files, also setup python virtual environment

--- backupmystuff.py
+++ backupmystuff.py
@@ -6,16 +6,19 @@ import shutil
 user = getpass.getuser()
 
 '''
+'''
+'''
 BACKUP PLASMA CONFIG FILES
+'''
+'''
 '''
 
 # Check if destination directory exists, if not then create it
-plasma_backup_path = Path(f"/home/{user}/.backups/plasma")
-if not plasma_backup_path.exists():
-    print(f"'/home/{user}/.plasma-cfg-backup' does not exist.  Creating it now.")
+mangohud_backup_path = Path(f"/home/{user}/.backups/plasma")
+if not mangohud_backup_path.exists():
+    print(f"'/home/{user}/.backups/plasma/' does not exist. Creating it now.")
     try:
-        plasma_backup_path.mkdir(parents = True, exist_ok = True)
-        print("Plasma backup directory created.")
+        mangohud_backup_path.mkdir(parents = True, exist_ok = True)
     except OSError as e:
         print(f"Error creating directory: {e}")
 
@@ -27,36 +30,40 @@ files = [file.name for file in cfg_path.iterdir() if file.is_file() and "plasma"
 readme_content = []
 
 # Readme for .config files
-readme_content.append(f"Config files in the main directory (.backups/plasma/) go in: /home/user/.config/\n\n")
+readme_content.append(f"Config files in the main directory (.backups/plasma/) go in: /home/user/.config/\n")
 
 # BACKUP .CONFIG FILES
 for file in files:
     src = f"{str(cfg_path)}/{file}"
-    shutil.copy(src, str(plasma_backup_path))
+    shutil.copy(src, str(mangohud_backup_path))
 
 # BACKUP PLASMA COLORS
 colors_path = Path(f"/home/{user}/.local/share/color-schemes")
 if colors_path.exists():
-    colors_plasma_backup_path = Path(f"{str(plasma_backup_path)}/color-schemes")
+    colors_mangohud_backup_path = Path(f"{str(mangohud_backup_path)}/color-schemes")
 
-    colors_plasma_backup_path.mkdir(parents = True, exist_ok = True)
+    colors_mangohud_backup_path.mkdir(parents = True, exist_ok = True)
     color_files = [file.name for file in colors_path.iterdir() if file.is_file()]
     for file in color_files:
         color_src = f"{str(colors_path)}/{file}"
-        shutil.copy(color_src, str(colors_plasma_backup_path))
+        shutil.copy(color_src, str(colors_mangohud_backup_path))
 
-    readme_content.append("COLORS:\ncolor-schemes directory: /home/user/.local/share/color-schemes\nMove backed up color files to this directory on your machine.\n\n")
+    readme_content.append("\nCOLORS:\ncolor-schemes directory: /home/user/.local/share/color-schemes\nMove backed up color files to this directory on your machine.\n")
 
 # Build readme file
 if len(readme_content) > 0:
-    readme_path = f"{str(plasma_backup_path)}/readme.txt"
+    readme_path = f"{str(mangohud_backup_path)}/readme.txt"
     with open(readme_path, "w") as file:
         file.write("".join(readme_content))
 
 print("Successfully backed up plasma config files.")
 
 '''
+'''
+'''
 BACKUP PS1 VARIABLE FOR KONSOLE
+'''
+'''
 '''
 
 ps1 = ""
@@ -76,13 +83,49 @@ with open(f"/home/{user}/.bashrc", 'r') as file:
 
 if len(ps1) > 0:
     # Create ps1 backup path
-    ps1_backup_path = Path(f"/home/{user}/.backups/ps1.txt")
+    ps1_backup_path = Path(f"/home/{user}/.backups")
     if not ps1_backup_path.exists():
+        print(f"'/home/{user}/.backups/' does not exist. Creating it now.")
         try:
             ps1_backup_path.mkdir(parents=True, exist_ok=True)
         except OSError as e:
             print(f"Error creating directory: {e}")
 
     # Create ps1 backup text file
-    with open(ps1_backup_path, "w") as file:
-        file.write(f"PS1:\n{ps1}\n\nThis variable is set in ~/.bashrc PS1='yourPS1'")
+    with open(f"{str(ps1_backup_path)}/ps1.txt", "w") as file:
+        file.write(f"PS1:\n{ps1}\nThis variable is set in ~/.bashrc PS1='yourPS1'\n")
+
+    print("Successfully backed up PS1 variable into ps1.txt.")
+
+'''
+'''
+'''
+BACKUP MANGOHUD CONFIG FILES
+'''
+'''
+'''
+
+# Check if destination directory exists, if not then create it
+mangohud_backup_path = Path(f"/home/{user}/.backups/mangohud")
+if not mangohud_backup_path.exists():
+    print(f"'/home/{user}/.backups/mangohud/' does not exist. Creating it now.")
+    try:
+        mangohud_backup_path.mkdir(parents=True, exist_ok=True)
+    except OSError as e:
+        print(f"Error creating directory: {e}")
+
+# Get list of mangohud cfg filenames from ~/.config/MangoHud directory
+mango_cfg_path = Path(f"/home/{user}/.config/MangoHud")
+mango_files = [file.name for file in mango_cfg_path.iterdir() if file.is_file()]
+
+# Backup config files
+for file in mango_files:
+    src = f"{str(mango_cfg_path)}/{file}"
+    shutil.copy(src, str(mangohud_backup_path))
+
+# Mangohud backup readme
+if len(mango_files) > 0:
+    with open(f"{str(mango_cfg_path)}/readme.txt", "w") as file:
+        file.write(f"MangoHud config files go in: ~/.config/MangoHud/\n")
+
+print("Successfully backed up MangoHud config files.")
